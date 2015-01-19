986375c67fe0e8696d411f643928ae5afde0c40f
Theodore Tripp <tjtripp@gmail.com>
2015-01-19 16:42:02 +0000

    added count_in_list.rb

--- exercises/count_in_list.rb
+++ exercises/count_in_list.rb
@@ -19,9 +19,26 @@ def count_in_list(list, item_to_count)
   #  1. A running total of the number of times you've seen the item
   #  2. A way to loop/iterate through the list
   #  3. A way to add to the running total as you see the item
+  
+  running_total = 0
+  list.each do |item|
+  # add .downcase if I want to ignore case.
+    if item == item_to_count  # should I use .to_s - only if I want "1" == 1 to yield true. 
+  
+      running_total += 1
+    end
+  end
+  running_total #return the total of item_to_count found in list
 end
+
 
 if __FILE__ == $0
   # I'd advise putting some sanity checks here.
   # How else will you be sure your code does what you think it does?
+  
+  p count_in_list([1, 2, 1, 4], 1) == 2 # integers
+  p count_in_list(["Jim", "Apple", "Orange", "Jim"], "Jim") == 2 # strings
+  p count_in_list(["1", 1, 1, 4], 1) == 2 # mix of str and int
+  p count_in_list([1, 1.0, 1.001, 315], 1) == 2 # int and float - 1 == 1.0 is true
+  p count_in_list(["", 100, 101, "100"], "") == 1 # empty item
 end
