14afaff7e5f00f14b10b9f69ac6f0f4f5e1158ac
Tanner Welsh <tannerwelsh@gmail.com>
2015-01-21 23:20:16 -0500

    Add implementation for longest_string() method

--- exercises/longest_string.rb
+++ exercises/longest_string.rb
@@ -12,10 +12,25 @@
 #   str.length == 4
 
 def longest_string(list)
-  # This is your job. :)
+  longest_so_far = list.first
+
+  list.each do |string|
+    if string.length >= longest_so_far.length
+      longest_so_far = string
+    end
+  end
+
+  return longest_so_far
 end
 
 if __FILE__ == $0
   # I'd advise putting some sanity checks here.
   # How else will you be sure your code does what you think it does?
+  p longest_string(["a"]) == "a"
+  p longest_string(["xyz", "xy", "x"]) == "xyz"
+  p longest_string(["xy", "x", "xyz"]) == "xyz"
+  p longest_string([""]) == ""
+  p longest_string(["bat", "cat", "to"]) == "cat"
+  p longest_string(["b", "bat", "to", "cat"]) == "cat"
+  p longest_string([]) == nil
 end
