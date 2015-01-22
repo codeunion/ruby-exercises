dbc1a9187b9ef00667e5ae4406607d97fc3d5379
Theodore Tripp <tjtripp@gmail.com>
2015-01-22 23:20:34 +0000

    Updated word_count.rb to split on non-word characters.

--- exercises/word_count.rb
+++ exercises/word_count.rb
@@ -7,30 +7,23 @@
 # that sequence of spaces in a sentence designates the start of a new word.
 # That means we don't care about how many spaces are between two words, only
 # that there is at least one.
-
 def word_count(string)
-  # Hint: You'll want to use String#split
-  # See: http://www.ruby-doc.org/core-2.1.2/String.html#method-i-split
-  words = string.split(" ")
-  words.length
+  words = string.split(/\W+/) #split on any non-word char. Thanks Rubular.com!
+  #Using \b counts even spaces as "words".  Using \W counts ... as three words.
+  #I think \W is better for now. Otherwise... I wonder if \W+ would work at all? YES.
+    words.length #return length
 end
-
+#Tests:
 if __FILE__ == $0
-  p word_count("apple") == 1
-  p word_count("123...321") == 1
-  p word_count("one two three") == 3
-  p word_count("This sentence has five words.") == 5
-  p word_count("No-spaces-here") == 1
-  p word_count("") == 0  # Empty string
-  p word_count(" ") == 0 # A string consisting of a single space
-  
-  # It's probably worth having a few more sanity checks.
-  # What are some input strings that might be tricky to handle?
-  # How about where the answer to "How many words?" is less clear-cut?
-  
-  p word_count("one\n two three") == 3
-  p word_count("one\ntwo\nthree") == 3
-  p word_count("This    test    has many  spaces.") == 5
-  #having trouble thinking of anything else to test - .split handles extra whitespace and counts \n as whitespace.
-  
+  p word_count("apple")                   == 1
+  p word_count("123...321")               == 2
+  p word_count("one.two three")           == 3
+  p word_count("This////bit has fv wds.") == 5
+  p word_count("No-spaces!?-here")        == 3
+  p word_count("")                        == 0  # Empty string
+  p word_count(".... ")                   == 0 # A string consisting of a single space
+  p word_count("one\n-- two three")       == 3
+  p word_count("one\ntwo;;;\nthree")      == 3
+  p word_count("is;;;  ts    has many s.")== 5
+  p word_count("one\n two three")         == 3
 end
