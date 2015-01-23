3bc5ff4716054241797d84bef3cd33e1139b11ba
Theodore Tripp <tjtripp@gmail.com>
2015-01-23 02:32:06 +0000

    Use #downto method

--- exercises/print_pyramid.rb
+++ exercises/print_pyramid.rb
@@ -12,41 +12,26 @@
 # ***
 # **
 # *
-
 # This is how we require the print-triangle.rb file in the current folder.
 # We can now use the "print_triangle" and "print_line" methods we defined
 # there — as if we defined them here!
-
 require_relative './print_triangle'
-
 def print_pyramid(height)
   # This is your job. :)
   # Suggestion: you can call print_triangle to print out the first, "upward" half of the pyramid
   # You'll have to write code to print out the second, "downward" half of the pyramid.
-  
-  print_triangle(height)
-  until height == 0 do # I tried (height..1).each do |i| but it didn't seem to be decrementing like I hoped
-    height -= 1
-    print_line(height)
+  print_triangle(height) #method from ./print_triangle
+  (height-1).downto(1) do |i| #learned #downto
+    print_line(i) #method from ./print_triangle
   end
-  
-   
+  puts
 end
-
 if __FILE__ == $0
   # I'd advise putting some sanity checks here.
-  # How else will you be sure your code does what you think it does?
-  
   print_pyramid(1)
-  print "\n"
   print_pyramid(2)
-  print "\n"
   print_pyramid(3)
-  print "\n"
   print_pyramid(4)
-  print "\n"
   print_pyramid(5)
-  print "\n"
   print_pyramid(10)
-  
 end
