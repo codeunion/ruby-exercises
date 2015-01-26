b8c45b66cf2f5a6a307e37221da03c599f3575fe
Theodore Tripp <tjtripp@gmail.com>
2015-01-26 03:16:26 +0000

    commas.rb - finished exercise

--- exercises/commas.rb
+++ exercises/commas.rb
@@ -19,17 +19,47 @@
 # has a piece of paper with a comma-less number on it.  How would you decide to
 # insert the commas?  Which comma would you insert first?
 
+# MY NOTES:
+# convert incoming number to string
+# split string on each char
+# iterate through array from end downto 0
+# build new array
+# every after every three items add comma
+# join new array to string
+# return new array
+# for negative: check if incoming number is < 0, if so add "- to array[0]
+
 def commas(num)
+  string = num.abs.to_s
+  if string.length <= 3
+    return string
+  else
+    array_with_commas = []
+    array = string.split(//)
+    array_reversed = array.reverse
+    array_reversed.each_index do |i|
+      array_with_commas.push(array_reversed[i])
+      if i%3 == 2
+        array_with_commas.push(",")
+      end
+    end
+  end
+  if num < 0
+    array_with_commas.push("-")
+  end
+  return array_with_commas.reverse.join
 end
 
 if __FILE__ == $0
-  # What are the common cases?  What are the corner cases?
-  # Your sanity checks should look like
-  #   p commas(input) == ...expected return value...
+  p commas(1)     # == "1"
+  p commas(12)     # == "12"
+  p commas(123)     # == "123"
+  p commas(1234)    # == "1,234"
+  p commas(12345)   # == "12,345"
+  p commas(1234567) # == "1,234,567"
+  p commas(-1234)   # == "1,234"
+  p commas(-12345)  # == "12,345"
+  p commas(-1234567)# == "-1,234,567"
+
 end
 
-# Hint #1
-# Use .to_s to convert a number to a string.  That is,
-#
-#  5.to_s   == "5"
-#  100.to_s == "100"
