980debd13fec36c3a96f69c5ddb9512868030f86
Theodore Tripp <tjtripp@gmail.com>
2015-02-01 23:40:57 +0000

    finsh pig_latin.rb

--- exercises/pig_latin.rb
+++ exercises/pig_latin.rb
@@ -1,27 +1,29 @@
-# Method name: pig_latin
-# Inputs:      A string representing a single word (i.e., no spaces)
-# Returns:     The word translated into "pig latin"
-# Prints:      Nothing
-
-# Note #1
-# There are two rules for translating something into "pig latin":
-#   1. If a word begins with a consonant or sequence of consonants, move the
-#      sequence of consonants  to the end of the word and then append "ay"
-#   2. If a word begins with a vowel, just append "ay"
-#
-# See: http://en.wikipedia.org/wiki/Pig_Latin#Rules
-#
-# Here are some examples:
-# pig_latin("happy") == "appyhay"
-
 def pig_latin(word)
+  vowels = ["a", "e", "i", "o", "u"]
+  suffix = "ay"
+  consonant_array = []
+  pig_latin = []
+  char_array = word.split(//)
+
+  char_array.each do |letter|
+    if vowels.include?(letter)
+      pig_latin.push(letter)
+    elsif pig_latin.length == 0 # no vowel found yet
+        consonant_array.push(letter)
+    else
+        pig_latin.push(letter)
+    end
+  end
+
+  pig_latin += consonant_array
+
+  pig_latin.join + suffix
 end
 
 if __FILE__ == $0
   p pig_latin("happy") == "appyhay"
   p pig_latin("duck")  == "uckday"
-  p pig_latin("glove") == "oveglay" # Notice what happened to "gl"
-
+  p pig_latin("glove") == "oveglay"
   p pig_latin("egg")   == "eggay"
   p pig_latin("inbox") == "inboxay"
   p pig_latin("eight") == "eightay"
