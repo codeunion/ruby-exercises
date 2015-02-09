3227ea2d71941d8eb89d037861d772fbe2aa2a29
Theodore Tripp <tjtripp@gmail.com>
2015-02-09 15:55:38 +0000

    run_length_encode.rb - apply feedback - it feels wrong to init run_length to 1.

--- exercises/run_length_encode.rb
+++ exercises/run_length_encode.rb
@@ -1,27 +1,22 @@
+#Rather than keep track of comparison_char in this way, why not compare array[i] to array[i+1]?
+
 def run_length_encode(string)
   compressed_array = []
-  run_length = 0
+  run_length = 1 # First char always has run_length of at least 1.
   array = string.each_char.to_a
-  comparison_char = array.first
 
   array.each_index do |i|
-
-    if comparison_char == array[i]
+    if array[i] == array[i+1]
       run_length += 1
 
       if i == array.length-1
-        compressed_array.push(run_length.to_s + comparison_char)
+        compressed_array.push(run_length.to_s + array[i])
       end
 
     else
-      compressed_array.push(run_length.to_s + comparison_char)
+      compressed_array.push(run_length.to_s + array[i])
 
-      comparison_char = array[i]
       run_length = 1
-
-      if i == array.length-1
-        compressed_array.push(run_length.to_s + comparison_char)
-      end
     end
   end
 
@@ -30,7 +25,7 @@ end
 
 if __FILE__ == $0
   p run_length_encode("WWWWWWAAAAAAWWWWWWAAAAAABBBBBB") == "6W6A6W6A6B"
-  p run_length_encode("A") == "1A"
-  p run_length_encode("AB") == "1A1B"
-  p run_length_encode("Mississippi") == "1M1i2s1i2s1i2p1i"
+  p run_length_encode("A")                              == "1A"
+  p run_length_encode("AB")                             == "1A1B"
+  p run_length_encode("Mississippi")                    == "1M1i2s1i2s1i2p1i"
 end
